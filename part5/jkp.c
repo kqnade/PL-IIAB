4a4899ca454ab732e28fb7ada760ad85430ef07d
kqnade <git@k4na.de>
2024-05-22 21:34:27 +0900

    fix algo part5 jkp.c

--- part5/jkp.c
+++ part5/jkp.c
@@ -3,29 +3,26 @@
 #include <time.h>
 
 int main() {
-  char *hands[] = {"グー", NULL, "チョキ", NULL, NULL, "パー"};
-  int user_input, computer_hand;
-  srand(time(NULL));
+    char *hands[] = {"グー", NULL, "チョキ", NULL, NULL, "パー"};
+    int user_input, computer_hand;
+    srand(time(NULL));
 
-  while (1) {
-    computer_hand = (rand() % 3) * 2.5;
-    printf("ジャンケンポン {0, 2, 5} > ");
-    if (scanf("%d", &user_input) || user_input < 0 || user_input > 5 ||
-        hands[user_input] == NULL) {
-      printf("何だそれー？\t私は%s\tもう一度!!\n", hands[computer_hand]);
-      while (getchar() != '\n');
-      continue;
+    while (1) {
+        computer_hand = (rand() % 3) * 2.5;
+        printf("ジャンケンポン {0, 2, 5} > ");
+        if (scanf("%d", &user_input) != 1 || user_input < 0 || user_input > 5 || hands[user_input] == NULL) {
+            printf("何だそれー？ もう一度!!\n");
+            while (getchar() != '\n'); // 入力バッファをクリア
+            continue;
+        }
+        printf("あなたは%s 私は%s ", hands[user_input], hands[computer_hand]);
+        if (user_input == computer_hand) {
+            printf("アイコ\n");
+        } else {
+            int result = ((user_input - computer_hand + 6) / 2) % 3;
+            printf(result == 1 ? "私の勝ち\n" : "あなたの勝ち\n");
+        }
     }
-
-    printf("あなたは%s\t私は%s\t", hands[user_input], hands[computer_hand]);
-
-    if (user_input == computer_hand) {
-      printf("アイコ\n");
-    } else {
-      int result = (user_input - computer_hand + 6) % 6;
-      printf(result % 2 == 0 ? "あなたの勝ち\n" : "私の勝ち\n");
-    }
-  }
-
-  return 0;
+    return 0;
 }
+
