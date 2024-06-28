b99c9b275e88544c5d663daedde4b3f1e177ba21
kqnade <git@k4na.de>
2024-06-28 12:02:38 +0900

    add collatz

---
+++ part10/collatz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int collatz(int x, int count) {
+  /* debugger */
+  /* printf("%d\n", x); */
+  if (x==1 && count != 0) return count;
+  else if (x%2 == 0) return collatz(x/2, count+1);
+  return collatz(3*x+1, count+1);
+}
+
+int main() {
+  printf("%d\n", collatz(65536, 0));
+}
